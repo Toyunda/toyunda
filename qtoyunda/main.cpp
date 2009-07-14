205ffd4bfffd946856a71ee17c3199ff71d5f2a7
skarsnik <skarsnik@9828fe01-180b-43f6-95d2-27e6f8c4ea64>
2009-07-14 01:22:05 +0000

    Close ticket #5

--- qtoyunda/main.cpp
+++ qtoyunda/main.cpp
@@ -1,28 +1,33 @@
-
-
 #include <QApplication>
-#include <QObject>
-#include <QTextStream>
-QTextStream cout(stdout);
-QTextStream cerr(stderr);
-
-#include "toystream.h"
-#include "qxine.h"
-#include "qtoyosd.h"
-
-#ifdef Q_WS_X11
+#include "qtoyunda.h"
 #include <X11/extensions/Xrender.h>
-#endif
 
 int	main(int ac, char *ag[])
 {
+  QToyunda              *toyunda;
+  QApplication		*qapp;
+
+  qDebug() << "Parsing command line argument";
+  if (ac != 5) {
+    qCritical() << "Invalid arguments, syntax : qtoyunda playername renderername video subtitle";
+    exit(1);
+  }
+  QString		playerName = ag[1];
+  QString		rendererName = ag[2];
+  QString               videofile = ag[3];
+  QString		subfile = ag[4];
+
+  toyunda = new QToyunda(playerName, rendererName);
+// Special Initialisation for the qosd renderer on old QT 4.x version
 #ifdef Q_WS_X11
-   qWarning("Please make sure you're running a composition manager!");
+#if QT_VERSION < 0x040500
+  if (rendererName == "qosd") {
+    qWarning("Please make sure you're running a composition manager!");
     bool  argbVisual=false;
     Display *dpy = XOpenDisplay(0); // open default display
     if (!dpy) {
-        qWarning("Cannot connect to the X server");
-        exit(1);
+          qWarning("Cannot connect to the X server");
+          exit(1);
     }
 
     int screen = DefaultScreen(dpy);
@@ -31,53 +36,52 @@ int	main(int ac, char *ag[])
     int eventBase, errorBase;
 
     if (XRenderQueryExtension(dpy, &eventBase, &errorBase)) {
-        int nvi;
-        XVisualInfo templ;
-        templ.screen  = screen;
-        templ.depth   = 32;
-        templ.c_class = TrueColor;
-        XVisualInfo *xvi = XGetVisualInfo(dpy, VisualScreenMask |
+          int nvi;
+          XVisualInfo templ;
+          templ.screen  = screen;
+          templ.depth   = 32;
+          templ.c_class = TrueColor;
+          XVisualInfo *xvi = XGetVisualInfo(dpy, VisualScreenMask |
                                           VisualDepthMask |
                                           VisualClassMask, &templ, &nvi);
 
-        for (int i = 0; i < nvi; ++i) {
-            XRenderPictFormat *format = XRenderFindVisualFormat(dpy,
-                                                                xvi[i].visual);
-            if (format->type == PictTypeDirect && format->direct.alphaMask) {
-                visual = xvi[i].visual;
-                colormap = XCreateColormap(dpy, RootWindow(dpy, screen),
-                                           visual, AllocNone);
-                argbVisual=true;
-                break;
-            }
+          for (int i = 0; i < nvi; ++i) {
+          XRenderPictFormat *format = XRenderFindVisualFormat(dpy,
+                                                                  xvi[i].visual);
+          if (format->type == PictTypeDirect && format->direct.alphaMask) {
+                  visual = xvi[i].visual;
+                  colormap = XCreateColormap(dpy, RootWindow(dpy, screen),
+                                          visual, AllocNone);
+                  argbVisual=true;
+                  break;
+          }
 
-        }
+          }
     }
     if (argbVisual == true) {
-        qWarning("Found ARGB visual. Starting app...");
+          qWarning("Found ARGB visual. Starting app...");
     }
     else  {
-        qWarning("Couldn't find ARGB visual... Exiting.");
+          qWarning("Couldn't find ARGB visual... Exiting.");
+          exit(1);
     }
-
-
-  QApplication app(dpy, ac, ag,
-                     Qt::HANDLE(visual), Qt::HANDLE(colormap));
-#elif
-  QApplication app(ac, ag);
+    qapp = new QApplication(dpy, ac, ag,
+                  Qt::HANDLE(visual), Qt::HANDLE(colormap));
+  } else {
 #endif
-  cout << "parse toyunda file\n";
-  ToyStream nm;
-  nm.loadFile(QString(ag[1]));
-  QXine p;
-  p.init();
-  cout << "starting play\n";
-  QToyOSD osd;
-  osd.setStream(&nm);
-  p.start(QString(ag[2]));
-  QObject::connect(&p, SIGNAL(frameChanged(int )), &nm, SLOT(setCurrentFrame(int)));
-  QObject::connect(&nm, SIGNAL(subChanged()), &osd, SLOT(repaint()));
-  osd.show();
-  return app.exec();
-  return 1;
+#endif
+    qapp = new QApplication(ac, ag);
+#ifdef Q_WS_X11
+#if QT_VERSION < 0x040500
+  }
+#endif
+#endif
+
+  qDebug() << "Init toyunda";
+  toyunda->init();
+  qDebug() << "Load files";
+  toyunda->load(videofile, subfile);
+  qDebug() << "Play the video";
+  toyunda->play();
+  return qapp->exec();
 }
