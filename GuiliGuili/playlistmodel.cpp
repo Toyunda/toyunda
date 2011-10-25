6fad8bd7af5a8510a0557900b74063db74914a0d
skarsnik <skarsnik@9828fe01-180b-43f6-95d2-27e6f8c4ea64>
2011-10-25 09:31:38 +0000

    add license specification in each file and GPL3 file, closing ticket #19
    Remove kdevelop file

--- GuiliGuili/playlistmodel.cpp
+++ GuiliGuili/playlistmodel.cpp
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2011  <copyright holder> <email>
+    This file is part of GuiliGuili software
+    Copyright (C) 2011  Sylvain "Skarsnik" Colinet <scolinet@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- GuiliGuili/song.cpp
+++ GuiliGuili/song.cpp
@@ -1,6 +1,6 @@
 /*
-    Song class is part of the GuiliGuili project
-    Copyright (C) 2011  Sylvain "Skarsnik" Colinet scolinet@gmail.com
+    This file is part of GuiliGuili software
+    Copyright (C) 2011  Sylvain "Skarsnik" Colinet <scolinet@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
